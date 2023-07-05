55c45eeb2dda641b3badf9b3e443b10359149a98
AndrewPoppe <AndrewBPoppe@gmail.com>
2023-07-05 13:11:55 -0400

    refactor

--- ajax/assign_user.php
+++ ajax/assign_user.php
@@ -6,14 +6,14 @@ namespace YaleREDCap\SecurityAccessGroups;
 
 $scriptPath = $module->getSafePath('UserRights/assign_user.php', APP_PATH_DOCROOT);
 
-if ( $_SERVER["REQUEST_METHOD"] !== "POST" ) {
+if ( $_SERVER['REQUEST_METHOD'] !== 'POST' ) {
     require_once $scriptPath;
     exit;
 }
 
 $data     = filter_input_array(INPUT_POST, FILTER_SANITIZE_ENCODED);
-$username = $data["username"];
-$role_id  = $data["role_id"];
+$username = $data['username'];
+$role_id  = $data['role_id'];
 $sag_id   = $module->getUserSystemRole($username);
 $sag      = $module->getSystemRoleRightsById($sag_id);
 
@@ -40,37 +40,37 @@ $userExpired = $module->isUserExpired($username, $project_id);
 
 if ( $errors === false || $userExpired ) {
     $info = [
-        "project_id"       => $project_id,
-        "username"         => $username,
-        "role_id"          => $role_id,
-        "role_label"       => $role_label,
-        "unique_role_name" => $unique_role_name
+        'project_id'       => $project_id,
+        'username'         => $username,
+        'role_id'          => $role_id,
+        'role_label'       => $role_label,
+        'unique_role_name' => $unique_role_name
     ];
     ob_start(function ($str) use ($info, $module) {
         try {
-            $succeeded = strpos($str, "userSaveMsg darkgreen") !== false; // is there no better way?
+            $succeeded = strpos($str, 'userSaveMsg darkgreen') !== false; // is there no better way?
             $module->log('ok', [ 'str' => $str, 'succeeded' => $succeeded ]);
             if ( $succeeded ) {
-                $data_values = "user = '" . $info["username"] . "'\nrole = '" . $info["role_label"] . "'\nunique_role_name = '" . $info["unique_role_name"] . "'";
+                $dataValues = "user = '" . $info["username"] . "'\nrole = '" . $info["role_label"] . "'\nunique_role_name = '" . $info["unique_role_name"] . "'";
 
-                $logTable     = $module->framework->getProject($info["project_id"])->getLogTable();
+                $logTable     = $module->framework->getProject($info['project_id'])->getLogTable();
                 $sql          = "SELECT log_event_id FROM $logTable WHERE project_id = ? AND user = ? AND page = 'ExternalModules/index.php' AND object_type = 'redcap_user_rights' AND pk = ? AND event IN ('INSERT','UPDATE') AND description = 'Assign user to role' AND TIMESTAMPDIFF(SECOND,ts,NOW()) <= 10 ORDER BY ts DESC";
-                $params       = [ $info["project_id"], $module->framework->getUser()->getUsername(), $info["username"] ];
+                $params       = [ $info['project_id'], $module->framework->getUser()->getUsername(), $info['username'] ];
                 $result       = $module->query($sql, $params);
-                $log_event_id = intval($result->fetch_assoc()["log_event_id"]);
-                if ( $log_event_id != 0 ) {
-                    $module->query("UPDATE $logTable SET data_values = ? WHERE log_event_id = ?", [ $module->framework->escape($data_values), $log_event_id ]);
+                $logEventId = intval($result->fetch_assoc()['log_event_id']);
+                if ( $logEventId != 0 ) {
+                    $module->query("UPDATE $logTable SET data_values = ? WHERE log_event_id = ?", [ $module->framework->escape($dataValues), $logEventId ]);
                 } else {
                     \Logging::logEvent(
                         '',
-                        "redcap_user_rights",
-                        "INSERT",
-                        $info["username"],
-                        $data_values,
-                        "Assign user to role",
-                        "",
-                        "",
-                        "",
+                        'redcap_user_rights',
+                        'INSERT',
+                        $info['username'],
+                        $dataValues,
+                        'Assign user to role',
+                        '',
+                        '',
+                        '',
                         true,
                         null,
                         null,
@@ -79,13 +79,13 @@ if ( $errors === false || $userExpired ) {
                 }
             }
         } catch ( \Throwable $e ) {
-            $module->log("Error logging user role mapping", [ "error" => $e->getMessage() ]);
+            $module->log('Error logging user role mapping', [ 'error' => $e->getMessage() ]);
         }
         return $str;
     });
     require_once $scriptPath;
     ob_end_flush(); // End buffering and clean up
 } else {
-    echo json_encode($module->framework->escape([ "error" => true, "bad_rights" => [ "$username" => [ "SAG" => $sag["role_name"], "rights" => $bad_rights ] ], "role" => $role_name ]));
+    echo json_encode($module->framework->escape([ 'error' => true, 'bad_rights' => [ "$username" => [ 'SAG' => $sag['role_name'], 'rights' => $bad_rights ] ], 'role' => $role_name ]));
 }
 exit;
