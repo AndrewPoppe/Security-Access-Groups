7c448d6160f31f0681b5e62b06aee5d9cea24bc9
AndrewPoppe <AndrewBPoppe@gmail.com>
2023-07-05 11:28:23 -0400

    refactor

--- ajax/import_export_users.php
+++ ajax/import_export_users.php
@@ -6,7 +6,7 @@ namespace YaleREDCap\SecurityAccessGroups;
 
 $scriptPath = $module->getSafePath('UserRights/import_export_users.php', APP_PATH_DOCROOT);
 
-if ( $_SERVER["REQUEST_METHOD"] !== "POST" || !isset($_POST['csv_content']) ) {
+if ( $_SERVER['REQUEST_METHOD'] !== 'POST' || !isset($_POST['csv_content']) ) {
     require_once $scriptPath;
     exit;
 }
@@ -22,22 +22,22 @@ if ( isset($_POST['csv_content']) && $_POST['csv_content'] != '' ) {
         $sag      = $module->getSystemRoleRightsById($sag_id);
 
         if ( isset($this_user['forms']) && $this_user['forms'] != '' ) {
-            foreach ( explode(",", $this_user['forms']) as $this_pair ) {
-                list( $this_form, $this_right )  = explode(":", $this_pair, 2);
+            foreach ( explode(',', $this_user['forms']) as $this_pair ) {
+                list( $this_form, $this_right )  = explode(':', $this_pair, 2);
                 $this_user['form-' . $this_form] = $this_right;
             }
             unset($this_user['forms']);
         }
         if ( isset($this_user['forms_export']) && $this_user['forms_export'] != '' ) {
-            foreach ( explode(",", $this_user['forms_export']) as $this_pair ) {
-                list( $this_form, $this_right )         = explode(":", $this_pair, 2);
+            foreach ( explode(',', $this_user['forms_export']) as $this_pair ) {
+                list( $this_form, $this_right )         = explode(':', $this_pair, 2);
                 $this_user['export-form-' . $this_form] = $this_right;
             }
             unset($this_user['forms_export']);
         }
-        $this_user = array_filter($this_user, function ($value, $key) {
+        $this_user = array_filter($this_user, function ($value) {
             return $value != 0;
-        }, ARRAY_FILTER_USE_BOTH);
+        });
 
         $acceptable_rights = $module->getAcceptableRights($username);
         $current_rights    = $module->getCurrentRights($username, $module->framework->getProjectId()) ?? [];
@@ -49,26 +49,26 @@ if ( isset($_POST['csv_content']) && $_POST['csv_content'] != '' ) {
 
         // We ignore expired users, unless the request unexpires them
         $userExpired         = $module->isUserExpired($username, $module->framework->getProjectId());
-        $requestedExpiration = $this_user["expiration"];
+        $requestedExpiration = $this_user['expiration'];
         $requestedUnexpired  = empty($requestedExpiration) || (strtotime($requestedExpiration) >= strtotime('today'));
         $ignore              = $userExpired && !$requestedUnexpired;
 
         if ( !empty($these_bad_rights) && !$ignore ) {
             $bad_rights[$username] = [
-                "SAG"    => $sag["role_name"],
-                "rights" => $these_bad_rights
+                'SAG'    => $sag['role_name'],
+                'rights' => $these_bad_rights
             ];
         }
     }
 
     if ( empty($bad_rights) ) {
-        ob_start(function ($str) use ($all_current_rights, $module) {
+        ob_start(function () use ($all_current_rights, $module) {
             try {
-                $imported    = $_SESSION["imported"] === "users";
-                $error_count = sizeof($_SESSION["errors"]) ?? 0;
+                $imported    = $_SESSION['imported'] === 'users';
+                $error_count = sizeof($_SESSION['errors']) ?? 0;
                 $succeeded   = $imported && $error_count === 0;
                 if ( $succeeded ) {
-                    $data_values = "";
+                    $data_values = '';
                     $pid         = $module->framework->getProjectId();
                     $logTable    = $module->framework->getProject($pid)->getLogTable();
                     $sql         = "SELECT log_event_id FROM $logTable WHERE project_id = ? AND user = ? AND page = 'ExternalModules/index.php' AND object_type = 'redcap_user_rights' AND pk = ? AND event IN ('INSERT','UPDATE') AND TIMESTAMPDIFF(SECOND,ts,NOW()) <= 10 ORDER BY ts DESC";
@@ -76,26 +76,26 @@ if ( isset($_POST['csv_content']) && $_POST['csv_content'] != '' ) {
                     foreach ( $all_current_rights as $username => $current_rights ) {
                         $updated_rights = $module->getCurrentRights($username, $pid) ?? [];
                         $changes        = json_encode(array_diff_assoc($updated_rights, $current_rights), JSON_PRETTY_PRINT);
-                        $changes        = $changes === "[]" ? "None" : $changes;
+                        $changes        = $changes === '[]' ? 'None' : $changes;
                         $data_values    = "user = '$username'\nchanges = $changes\n\n";
 
                         $params       = [ $pid, $redcap_user, $username ];
                         $result       = $module->framework->query($sql, $params);
-                        $log_event_id = intval($result->fetch_assoc()["log_event_id"]);
+                        $log_event_id = intval($result->fetch_assoc()['log_event_id']);
 
                         if ( $log_event_id != 0 ) {
                             $module->framework->query("UPDATE $logTable SET data_values = ? WHERE log_event_id = ?", [ $data_values, $log_event_id ]);
                         } else {
                             \Logging::logEvent(
                                 '',
-                                "redcap_user_rights",
-                                "update",
+                                'redcap_user_rights',
+                                'update',
                                 $username,
                                 $data_values,
-                                "Update user",
-                                "",
-                                "",
-                                "",
+                                'Update user',
+                                '',
+                                '',
+                                '',
                                 true,
                                 null,
                                 null,
@@ -105,14 +105,14 @@ if ( isset($_POST['csv_content']) && $_POST['csv_content'] != '' ) {
                     }
                 }
             } catch ( \Throwable $e ) {
-                $module->log("Error logging user edit (csv import)", [ "error" => $e->getMessage() ]);
+                $module->log('Error logging user edit (csv import)', [ 'error' => $e->getMessage() ]);
             }
         });
-        $module->framework->log('User Rights Import: Importing users', [ "users" => json_encode($data) ]);
+        $module->framework->log('User Rights Import: Importing users', [ 'users' => json_encode($data) ]);
         require_once $scriptPath;
         ob_end_flush(); // End buffering and clean up
     } else {
-        $module->framework->log('User Rights Import: Bad rights found', [ "bad rights" => json_encode($bad_rights) ]);
+        $module->framework->log('User Rights Import: Bad rights found', [ 'bad rights' => json_encode($bad_rights) ]);
         $_SESSION['SUR_imported']   = 'users';
         $_SESSION['SUR_bad_rights'] = json_encode($bad_rights);
         redirect(APP_PATH_WEBROOT . 'UserRights/index.php?pid=' . PROJECT_ID);
