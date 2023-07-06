0e68c322259caac38d4806a3b323fa87351a66f5
AndrewPoppe <AndrewBPoppe@gmail.com>
2023-07-06 10:28:15 -0400

    refactor

--- ajax/users.php
+++ ajax/users.php
@@ -8,11 +8,11 @@ if ( !$module->framework->getUser()->isSuperUser() ) {
     exit;
 }
 
-if ( $_SERVER["REQUEST_METHOD"] !== "POST" ) {
+if ( $_SERVER['REQUEST_METHOD'] !== 'POST' ) {
     http_response_code(405);
     exit;
 }
 
 $users = $module->getAllUserInfo(true);
 
-echo json_encode([ "data" => $users ]);
+echo json_encode([ 'data' => $users ]);
