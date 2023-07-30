e311e0bf8183316245ab25e5ed25b23ba677324f
AndrewPoppe <AndrewBPoppe@gmail.com>
2023-07-30 16:22:25 -0400

    progress

--- classes/RightsChecker.php
+++ classes/RightsChecker.php
@@ -21,8 +21,8 @@ class RightsChecker
         $this->module           = $module;
         $this->rightsToCheck    = $rightsToCheck;
         $this->acceptableRights = $acceptableRights;
-        $this->dataViewing      = intval($acceptableRights["dataViewing"]);
-        $this->dataExport       = intval($acceptableRights["dataExport"]);
+        $this->dataViewing      = intval($acceptableRights['dataViewing']);
+        $this->dataExport       = intval($acceptableRights['dataExport']);
         $this->projectId        = $projectId;
         $this->project          = new SAGProject($this->module, $this->projectId);
         $this->checkAllRights   = $checkAllRights;
@@ -31,22 +31,22 @@ class RightsChecker
     private function isSafeRight($rightName)
     {
         $safeRights = [
-            "project_id",
-            "username",
-            "role_id",
-            "user",
-            "submit-action",
-            "role_name",
-            "role_name_edit",
-            "redcap_csrf_token",
-            "expiration",
-            "group_role",
-            "group_id",
-            "api_token",
-            "data_access_group_id",
-            "unique_role_name",
-            "role_label",
-            "notify_email"
+            'project_id',
+            'username',
+            'role_id',
+            'user',
+            'submit-action',
+            'role_name',
+            'role_name_edit',
+            'redcap_csrf_token',
+            'expiration',
+            'group_role',
+            'group_id',
+            'api_token',
+            'data_access_group_id',
+            'unique_role_name',
+            'role_label',
+            'notify_email'
         ];
         return in_array($rightName, $safeRights, true);
     }
@@ -60,98 +60,113 @@ class RightsChecker
 
     private function checkSurveyEditRights($right, $value)
     {
-        $isSurveyResponseEditingRight = substr_compare($right, "form-editresp-", 0, strlen("form-editresp-")) === 0;
+        $isSurveyResponseEditingRight = substr_compare($right, 'form-editresp-', 0, strlen('form-editresp-')) === 0;
         if ( !$isSurveyResponseEditingRight ) {
             return;
         }
         $this->accountedFor = true;
-        if ( $value == "on" && $this->dataViewing < 3 ) {
-            $this->badRights[] = "Data Viewing - Edit Survey Responses";
+        if ( $value == 'on' && $this->dataViewing < 3 ) {
+            $mainRight         = RightsUtilities::getDisplayTextForRight('dataViewing');
+            $secondaryRight    = RightsUtilities::getDisplayTextForRight('editSurveyResponses');
+            $this->badRights[] = $mainRight . ' - ' . $secondaryRight;
         }
     }
 
     private function checkDataViewingRights($right, $value)
     {
-        $isDataViewingRight = substr_compare($right, "form-", 0, strlen("form-")) === 0;
+        $isDataViewingRight = substr_compare($right, 'form-', 0, strlen('form-')) === 0;
         if ( !$isDataViewingRight ) {
             return;
         }
         $this->accountedFor = true;
+<<<<<<< HEAD
         // 0: no access, 2: read only, 1: view and edit, 3: edit survey responses
         if ( $value === '3' && $this->dataViewing < 3 ) {
             $this->badRights[] = "Data Viewing - Edit Survey Responses";
         } elseif ( $value === '1' && $this->dataViewing < 2 ) {
             $this->badRights[] = "Data Viewing - View & Edit";
+=======
+        $mainRight          = RightsUtilities::getDisplayTextForRight('dataViewing');
+        // 0: no access, 2: read only, 1: view and edit, 3: edit survey responses
+        if ( $value === '3' && $this->dataViewing < 3 ) {
+            $this->badRights[] = $mainRight . ' - ' . RightsUtilities::getDisplayTextForRight('editSurveyResponses');
+        } elseif ( $value === '1' && $this->dataViewing < 2 ) {
+            $this->badRights[] = $mainRight . ' - ' . RightsUtilities::getDisplayTextForRight('viewAndEdit');
+>>>>>>> progress
         } elseif ( $value === '2' && $this->dataViewing < 1 ) {
-            $this->badRights[] = "Data Viewing - Read Only";
+            $this->badRights[] = $mainRight . ' - ' . RightsUtilities::getDisplayTextForRight('readOnly');
         }
     }
 
     private function checkDataViewingRights2($right)
     {
-        $isDataViewingRight = substr_compare($right, "data_entry", 0, strlen("data_entry")) === 0;
+        $isDataViewingRight = substr_compare($right, 'data_entry', 0, strlen('data_entry')) === 0;
         if ( !$isDataViewingRight ) {
             return;
         }
         $this->accountedFor = true;
-        if ( $right === "data_entry3" && $this->dataViewing < 3 ) {
-            $this->badRights[] = "Data Viewing - Edit Survey Responses";
-        } elseif ( $right === "data_entry1" && $this->dataViewing < 2 ) {
-            $this->badRights[] = "Data Viewing - View & Edit";
-        } elseif ( $right === "data_entry2" && $this->dataViewing < 1 ) {
-            $this->badRights[] = "Data Viewing - Read Only";
+        $mainRight          = RightsUtilities::getDisplayTextForRight('dataViewing');
+        if ( $right === 'data_entry3' && $this->dataViewing < 3 ) {
+            $this->badRights[] = $mainRight . ' - ' . RightsUtilities::getDisplayTextForRight('editSurveyResponses');
+        } elseif ( $right === 'data_entry1' && $this->dataViewing < 2 ) {
+            $this->badRights[] = $mainRight . ' - ' . RightsUtilities::getDisplayTextForRight('viewAndEdit');
+        } elseif ( $right === 'data_entry2' && $this->dataViewing < 1 ) {
+            $this->badRights[] = $mainRight . ' - ' . RightsUtilities::getDisplayTextForRight('readOnly');
         }
     }
 
     private function checkDataExportRights($right, $value)
     {
-        $isDataExportRight = substr_compare($right, "export-form-", 0, strlen("export-form-")) === 0;
+        $isDataExportRight = substr_compare($right, 'export-form-', 0, strlen('export-form-')) === 0;
         if ( !$isDataExportRight ) {
             return;
         }
         $this->accountedFor = true;
+        $mainRight          = RightsUtilities::getDisplayTextForRight('dataExport');
         // 0: no access, 2: deidentified, 3: remove identifiers, 1: full data set
         if ( $value === '1' && $this->dataExport < 3 ) {
-            $this->badRights[] = "Data Export - Full Data Set";
+            $this->badRights[] = $mainRight . ' - ' . RightsUtilities::getDisplayTextForRight('fullDataSet');
         } elseif ( $value === '3' && $this->dataExport < 2 ) {
-            $this->badRights[] = "Data Export - Remove Identifiers";
+            $this->badRights[] = $mainRight . ' - ' . RightsUtilities::getDisplayTextForRight('removeIdentifiers');
         } elseif ( $value === '2' && $this->dataExport < 1 ) {
-            $this->badRights[] = "Data Export - De-Identified";
+            $this->badRights[] = $mainRight . ' - ' . RightsUtilities::getDisplayTextForRight('deidentified');
         }
     }
 
     private function checkDataExportRights2($right)
     {
-        $isDataExportRight = substr_compare($right, "data_export", 0, strlen("data_export")) === 0;
+        $isDataExportRight = substr_compare($right, 'data_export', 0, strlen('data_export')) === 0;
         if ( !$isDataExportRight ) {
             return;
         }
         $this->accountedFor = true;
+        $mainRight          = RightsUtilities::getDisplayTextForRight('dataExport');
         // 0: no access, 2: deidentified, 3: remove identifiers, 1: full data set
-        if ( $right === "data_export1" && $this->dataExport < 3 ) {
-            $this->badRights[] = "Data Export - Full Data Set";
-        } elseif ( $right === "data_export3" && $this->dataExport < 2 ) {
-            $this->badRights[] = "Data Export - Remove Identifiers";
-        } elseif ( $right === "data_export2" && $this->dataExport < 1 ) {
-            $this->badRights[] = "Data Export - De-Identified";
+        if ( $right === 'data_export1' && $this->dataExport < 3 ) {
+            $this->badRights[] = $mainRight . ' - ' . RightsUtilities::getDisplayTextForRight('fullDataSet');
+        } elseif ( $right === 'data_export3' && $this->dataExport < 2 ) {
+            $this->badRights[] = $mainRight . ' - ' . RightsUtilities::getDisplayTextForRight('removeIdentifiers');
+        } elseif ( $right === 'data_export2' && $this->dataExport < 1 ) {
+            $this->badRights[] = $mainRight . ' - ' . RightsUtilities::getDisplayTextForRight('deidentified');
         }
     }
 
     private function checkRecordLockingRights($right, $value)
     {
-        $isRecordLockRight = $right == "lock_record";
+        $isRecordLockRight = $right == 'lock_record';
         if ( !$isRecordLockRight ) {
             return;
         }
         $this->accountedFor = true;
         if ( intval($value) > intval($this->acceptableRights[$right]) ) {
-            $this->badRights[] = "Record Locking" . ($value == 2 ? " with E-signature" : "");
+            $rightName         = $value == 2 ? 'lock_record_esignature' : 'lock_record';
+            $this->badRights[] = RightsUtilities::getDisplayTextForRight($rightName);
         }
     }
 
     private function checkDoubleDataRights($right, $value)
     {
-        $isDoubleDataRight = $right == "double_data";
+        $isDoubleDataRight = $right == 'double_data';
         if ( !$isDoubleDataRight ) {
             return;
         }
@@ -164,18 +179,19 @@ class RightsChecker
         // 0: reviewer
         // 1: double data person 1
         // 2: double data person 2
-        $ddeReview = $this->acceptableRights["double_data_reviewer"] == 1;
-        $ddePerson = $this->acceptableRights["double_data_person"] == 1;
+        $ddeReview = $this->acceptableRights['double_data_reviewer'] == 1;
+        $ddePerson = $this->acceptableRights['double_data_person'] == 1;
         if ( intval($value) == 0 && !$ddeReview ) {
-            $this->badRights[] = "Double Data Entry Reviewer";
+            $this->badRights[] = RightsUtilities::getDisplayTextForRight('double_data_reviewer');
         } elseif ( intval($value) > 0 && !$ddePerson ) {
-            $this->badRights[] = "Double Data Entry Person";
+            $this->badRights[] = RightsUtilities::getDisplayTextForRight('double_data_person');
+            ;
         }
     }
 
     private function checkDataResolutionRights($right, $value)
     {
-        $isDataResolutionRight = $right == "data_quality_resolution";
+        $isDataResolutionRight = $right == 'data_quality_resolution';
         if ( !$isDataResolutionRight ) {
             return;
         }
@@ -196,6 +212,7 @@ class RightsChecker
         $dqrRespond = $this->acceptableRights['data_quality_resolution_respond'] == 1;
         $dqrClose   = $this->acceptableRights['data_quality_resolution_close'] == 1;
 
+<<<<<<< HEAD
         if ( $value != '4' && !$dqrView ) {
             $badRight          = RightsUtilities::getDisplayTextForRight('data_quality_resolution_view');
             $this->badRights[] = $badRight;
@@ -209,6 +226,18 @@ class RightsChecker
             $this->badRights[] = $badRight;
         }
         if ( $value == '3' && !$dqrClose ) {
+=======
+        if ( $value == '1' && !$dqrView ) {
+            $badRight          = RightsUtilities::getDisplayTextForRight('data_quality_resolution_view');
+            $this->badRights[] = $badRight;
+        } elseif ( ($value == '4' || $value == '5' || $value == '3') && !$dqrOpen ) {
+            $badRight          = RightsUtilities::getDisplayTextForRight('data_quality_resolution_open');
+            $this->badRights[] = $badRight;
+        } elseif ( ($value == '2' || $value == '5' || $value == '3') && !$dqrRespond ) {
+            $badRight          = RightsUtilities::getDisplayTextForRight('data_quality_resolution_respond');
+            $this->badRights[] = $badRight;
+        } elseif ( $value == '3' && !$dqrClose ) {
+>>>>>>> progress
             $badRight          = RightsUtilities::getDisplayTextForRight('data_quality_resolution_close');
             $this->badRights[] = $badRight;
         }
