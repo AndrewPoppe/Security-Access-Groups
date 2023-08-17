c17cd244d21db5dee7e862445c37035402a4949c
AndrewPoppe <AndrewBPoppe@gmail.com>
2023-08-17 15:54:17 -0400

    complete

--- docs/createDocsImages.js
+++ docs/createDocsImages.js
@@ -13,7 +13,7 @@ const urlBase = 'http://localhost:13740';
 const redcapVersion = 'redcap_v13.1.27';
 const screenshotsPath = `screenshots`;
 
-const shotsToTake = "project"; // "project", "control_center", "other", "all"
+const shotsToTake = "all"; // "project", "control_center", "other", "all"
 
 const languages = [
     { selectValue: "", label: "English", code: "EN" },
@@ -62,7 +62,7 @@ const other_config_options = {
 };
 
 
-(async() => {
+(async () => {
     const browser = await playwright.chromium.launch({ headless: true });
     const context = await browser.newContext({ viewport: { width: 1865, height: 947 } });
     await context.addInitScript({
@@ -76,6 +76,8 @@ const other_config_options = {
     await page.locator('#password').fill('password');
 
     await page.click('#login_btn');
+
+
 
     for (const language of languages) {
         console.log(language.label);
@@ -344,6 +346,154 @@ const other_config_options = {
             const p_status_alert_user_rights_holder_reminder = await page.screenshot({ clip: { x: box_p_status_alert_user_rights_holder.x - 30, y: box_p_status_alert_user_rights_holder.y - 20, width: box_p_status_alert_user_rights_holder.width + 60, height: box_p_status_alert_user_rights_holder.height + 40 } });
             await sharp(p_status_alert_user_rights_holder_reminder).extend(project_config_options).toFile(`${screenshotsPath}/${language.code}/p_status_alert_user_rights_holder_reminder.png`);
             await page.locator('div#emailUserRightsHoldersModal button.btn-close').click();
+
+            // SHOT: p_status_expiration
+            console.log('\tp_status_expiration');
+            await page.evaluate(() => { window.shown = false; });
+            const waitForModal_p_expiration = page.waitForFunction(() => { $('#userExpirationModal').on('shown.bs.modal', () => window.shown = true); return window.shown; });
+            await page.locator('div#containerCard div.buttonContainer button.btn.btn-danger').click();
+            await waitForModal_p_expiration;
+            const box_p_status_expiration = await page.locator('div#userExpirationModal div.modal-content').boundingBox();
+            const p_status_expiration = await page.screenshot({ clip: { x: box_p_status_expiration.x - 30, y: box_p_status_expiration.y - 20, width: box_p_status_expiration.width + 60, height: box_p_status_expiration.height + 40 } });
+            await sharp(p_status_expiration).extend(project_config_options).toFile(`${screenshotsPath}/${language.code}/p_status_expiration.png`);
+
+            // SHOT: p_status_expiration_alert_user
+            console.log('\tp_status_expiration_alert_user');
+            await page.locator('div#userExpirationModal input#sendUserNotification').check();
+            await page.waitForTimeout(500);
+            const button_p_status_expiration_alert_user = page.locator('div#userExpirationModal div#userNotificationInfo button.btn.btn-info');
+            const box_p_status_expiration_user = await page.locator('div#userExpirationModal div.modal-content').boundingBox();
+            await button_p_status_expiration_alert_user.waitFor('visible');
+            await button_p_status_expiration_alert_user.scrollIntoViewIfNeeded();
+            const p_status_expiration_alert_user = await page.screenshot({ clip: { x: box_p_status_expiration.x - 30, y: box_p_status_expiration.y - 20, width: box_p_status_expiration_user.width + 60, height: box_p_status_expiration_user.height + 40 } });
+            await sharp(p_status_expiration_alert_user).extend(project_config_options).toFile(`${screenshotsPath}/${language.code}/p_status_expiration_alert_user.png`);
+
+            // SHOT: p_status_expiration_alert_user-rights-holder
+            console.log('\tp_status_expiration_alert_user-rights-holder');
+            await page.locator('div#userExpirationModal input#sendNotification-userExpiration-UserRightsHolders').check();
+            await page.waitForTimeout(500);
+            const text_p_status_expiration_alert_user_rights_holder = page.locator('div#userExpirationModal div#notificationInfo-userExpiration-UserRightsHolders code', { hasText: "[sag-users]" });
+            const box_p_status_expiration_user_rights_holder = await page.locator('div#userExpirationModal div.modal-content').boundingBox();
+            await text_p_status_expiration_alert_user_rights_holder.waitFor('visible');
+            await page.evaluate(() => { $('div#userExpirationModal .modal-body')[0].scrollTo(0, $('#expireUsersUserRightsHolderSelection')[0].getBoundingClientRect().top + 200); });
+            await page.waitForTimeout(500);
+            const p_status_expiration_alert_user_rights_holder = await page.screenshot({ clip: { x: box_p_status_expiration_user_rights_holder.x - 30, y: box_p_status_expiration_user_rights_holder.y - 20, width: box_p_status_expiration_user_rights_holder.width + 60, height: box_p_status_expiration_user_rights_holder.height + 40 } });
+            await sharp(p_status_expiration_alert_user_rights_holder).extend(project_config_options).toFile(`${screenshotsPath}/${language.code}/p_status_expiration_alert_user-rights-holder.png`);
+
+            // SHOT p_alert_log
+            console.log('\tp_alert_log');
+            const dtInitPromise_p_alerts = page.waitForFunction(() => { if ($.fn.dataTable.isDataTable('#alertLogTable')) return $('#alertLogTable').DataTable().data().count() > 0; });
+            await page.goto(`${urlBase}/${redcapVersion}/ExternalModules/?prefix=security_access_groups&page=project-alert-log&pid=${projectId}`);
+            await dtInitPromise_p_alerts;
+            await page.evaluate(() => { window.scrollTo(0, $('.SAG-Container')[0].getBoundingClientRect().top - 20) });
+            const box_p_alert_log = await page.locator('div.SAG-Container').boundingBox();
+            const box_p_alert_log_wrapper = await page.locator('div.alertLogWrapper').boundingBox();
+            const p_alert_log = await page.screenshot({
+                clip: {
+                    x: box_p_alert_log.x - 20,
+                    y: box_p_alert_log.y - 20,
+                    width: box_p_alert_log_wrapper.width + 40,
+                    height: box_p_alert_log.height + 40
+                }
+            });
+            await sharp(p_alert_log).extend(project_config_options).toFile(`${screenshotsPath}/${language.code}/p_alert_log.png`);
+
+            // SHOT: p_logs_preview
+            console.log('\tp_logs_preview');
+            await page.evaluate(() => { sag_module.openAlertPreview(37212); });
+            await page.locator('#alertPreviewModal').waitFor('visible');
+            await page.waitForTimeout(500);
+            const box_alert_preview_modal = await page.locator('#alertPreviewModal .modal-content').boundingBox();
+            const p_logs_preview = await page.screenshot({ clip: { x: box_alert_preview_modal.x - 30, y: box_alert_preview_modal.y - 20, width: box_alert_preview_modal.width + 60, height: box_alert_preview_modal.height + 40 } });
+            await sharp(p_logs_preview).extend(project_config_options).toFile(`${screenshotsPath}/${language.code}/p_logs_preview.png`);
+
+
+            // SHOT: p_blocked
+            console.log('\tp_blocked');
+            await page.goto(`${urlBase}/${redcapVersion}/UserRights/index.php?pid=${projectId}`);
+            await page.waitForURL(`${urlBase}/${redcapVersion}/UserRights/index.php?pid=${projectId}`);
+            await page.locator('a.userRightsExpired[userid="bob"]').click();
+            await page.locator('div#userClickExpiration').waitFor('visible');
+            await page.locator('input#tooltipExpiration').clear();
+            await page.locator('button#tooltipExpirationBtn').click();
+            await page.locator('div.swal2-popup.swal2-show').waitFor('visible');
+            await page.waitForTimeout(500);
+            const box_p_blocked = await page.locator('div.swal2-popup.swal2-show').boundingBox();
+            const p_blocked = await page.screenshot({ clip: { x: box_p_blocked.x - 30, y: box_p_blocked.y - 20, width: box_p_blocked.width + 60, height: box_p_blocked.height + 40 } });
+            await sharp(p_blocked).extend(project_config_options).toFile(`${screenshotsPath}/${language.code}/p_blocked.png`);
+
+            // SHOT: p_logging_user
+            console.log('\tp_logging_user');
+            const log_url1 = `${urlBase}/${redcapVersion}/Logging/index.php?pid=${projectId}&beginTime=07/23/2023%2018:20&endTime=07/23/2023%2018:22&usr=admin&record=&logtype=user&dag=`;
+            await page.goto(log_url1);
+            await page.waitForURL(log_url1, { waitUntil: 'networkidle' });
+            const td_log_user = await page.locator('td[ts="20230723182103"]').first().boundingBox();
+            const box_log_user = await page.locator('table[logeventtable="redcap_log_event6"]').boundingBox();
+            const p_logging_user = await page.screenshot({ clip: { x: box_log_user.x, y: td_log_user.y, width: box_log_user.width, height: td_log_user.height } });
+            await sharp(p_logging_user).extend(project_config_options).toFile(`${screenshotsPath}/EN/p_logging_user.png`);
+
+            // SHOT: p_logging_role
+            console.log('\tp_logging_role');
+            const log_url2 = `${urlBase}/${redcapVersion}/Logging/index.php?pid=${projectId}&beginTime=07/21/2023%2016:41&endTime=07/21/2023%2016:43&usr=admin&record=&logtype=user&dag=`;
+            await page.goto(log_url2);
+            await page.waitForURL(log_url2, { waitUntil: 'networkidle' });
+            const td_log_role = await page.locator('td[ts="20230721164250"]').first().boundingBox();
+            const box_log_role = await page.locator('table[logeventtable="redcap_log_event6"]').boundingBox();
+            const p_logging_role = await page.screenshot({ clip: { x: box_log_role.x, y: td_log_role.y, width: box_log_role.width, height: td_log_role.height } });
+            await sharp(p_logging_role).extend(project_config_options).toFile(`${screenshotsPath}/EN/p_logging_role.png`);
+        }
+
+        ///////////////////
+        //     Other     //
+        ///////////////////
+
+        if (shotsToTake === "other" || shotsToTake === "all") {
+            console.log('  --other--');
+
+            // SHOT: searching_example
+            console.log('\tsearching_example');
+            const dtInitPromise_searching = page.waitForFunction(() => { if ($.fn.dataTable.isDataTable('#SAG-System-Table')) return $('#SAG-System-Table').DataTable().data().count() > 0; });
+            await page.goto(`${urlBase}/${redcapVersion}/ExternalModules/?prefix=security_access_groups&page=system-settings-userlist`);
+            await dtInitPromise_searching;
+            await page.locator('div.SAG_Container div.dataTables_filter input').fill('Nothing');
+            await page.waitForTimeout(500);
+            const box_searching_example = await page.locator('div.SAG_Container div.card').boundingBox();
+            const searching_example = await page.screenshot({ clip: { x: box_searching_example.x - 20, y: box_searching_example.y - 20, width: box_searching_example.width + 40, height: box_searching_example.height + 40 } });
+            await sharp(searching_example).extend(other_config_options).toFile(`${screenshotsPath}/${language.code}/searching_example.png`);
+
+
+            // SHOT: searching_example_AND
+            console.log('\tsearching_example_AND');
+            await page.locator('div.SAG_Container div.dataTables_filter input').fill('joe admin');
+            await page.waitForTimeout(500);
+            const box_searching_example_AND = await page.locator('div.SAG_Container div.card').boundingBox();
+            const searching_example_AND = await page.screenshot({ clip: { x: box_searching_example_AND.x - 20, y: box_searching_example_AND.y - 20, width: box_searching_example_AND.width + 40, height: box_searching_example_AND.height + 40 } });
+            await sharp(searching_example_AND).extend(other_config_options).toFile(`${screenshotsPath}/${language.code}/searching_example_AND.png`);
+
+            // SHOT: searching_example_exact_phrase
+            console.log('\tsearching_example_exact_phrase');
+            await page.locator('div.SAG_Container div.dataTables_filter input').fill('"joe admin"');
+            await page.waitForTimeout(500);
+            const box_searching_example_exact_phrase = await page.locator('div.SAG_Container div.card').boundingBox();
+            const searching_example_exact_phrase = await page.screenshot({ clip: { x: box_searching_example_exact_phrase.x - 20, y: box_searching_example_exact_phrase.y - 20, width: box_searching_example_exact_phrase.width + 40, height: box_searching_example_exact_phrase.height + 40 } });
+            await sharp(searching_example_exact_phrase).extend(other_config_options).toFile(`${screenshotsPath}/${language.code}/searching_example_exact_phrase.png`);
+
+            // SHOT: searching_example_OR
+            console.log('\tsearching_example_OR');
+            await page.locator('div.SAG_Container div.dataTables_filter input').fill('alice | bob');
+            await page.waitForTimeout(500);
+            const box_searching_example_OR = await page.locator('div.SAG_Container div.card').boundingBox();
+            const searching_example_OR = await page.screenshot({ clip: { x: box_searching_example_OR.x - 20, y: box_searching_example_OR.y - 20, width: box_searching_example_OR.width + 40, height: box_searching_example_OR.height + 40 } });
+            await sharp(searching_example_OR).extend(other_config_options).toFile(`${screenshotsPath}/${language.code}/searching_example_OR.png`);
+
+            // SHOT: searching_example_regular_expression
+            console.log('\tsearching_example_regular_expression');
+            await page.locator('div.SAG_Container div.dataTables_filter input').fill('| d[a-z]*n');
+            await page.waitForTimeout(500);
+            const box_searching_example_regular_expression = await page.locator('div.SAG_Container div.card').boundingBox();
+            const searching_example_regular_expression = await page.screenshot({ clip: { x: box_searching_example_regular_expression.x - 20, y: box_searching_example_regular_expression.y - 20, width: box_searching_example_regular_expression.width + 40, height: box_searching_example_regular_expression.height + 40 } });
+            await sharp(searching_example_regular_expression).extend(other_config_options).toFile(`${screenshotsPath}/${language.code}/searching_example_regular_expression.png`);
+
         }
     }
     // Go to Control Center
